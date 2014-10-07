7ddd655d8d5fa124af5ae03968c89603aa70b1cc
Daniel Ku <kjunine@gmail.com>
2014-10-07 14:03:55 +0900

    Adds initial version.

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'docker-nginx'
+maintainer       'Daniel Ku'
+maintainer_email 'kjunine@gmail.com'
+license          'MIT'
+description      'Installs/Configures docker-nginx'
+long_description 'Installs/Configures docker-nginx'
+version          '0.1.0'
+
